3cff83b795c24f39ff051c47ac529b83ff0fed0b
JuanY <JuanY@LAPTOP-1J09QLAE>
2021-12-14 21:19:18 -0600

    futhd

---
+++ src/pojos/equipo.java
@@ -0,0 +1,73 @@
+package pojos;
+public class equipo {
+    
+    int id;
+    String equipo;
+    int ganotados;
+    int pganados;
+    int pempatados;
+    int pperdidos;
+    int ptotales;    
+    
+    public equipo(){
+    
+}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(String equipo) {
+        this.equipo = equipo;
+    }
+
+    public int getGanotados() {
+        return ganotados;
+    }
+
+    public void setGanotados(int ganotados) {
+        this.ganotados = ganotados;
+    }
+
+    public int getPganados() {
+        return pganados;
+    }
+
+    public void setPganados(int pganados) {
+        this.pganados = pganados;
+    }
+
+    public int getPempatados() {
+        return pempatados;
+    }
+
+    public void setPempatados(int pempatados) {
+        this.pempatados = pempatados;
+    }
+
+    public int getPperdidos() {
+        return pperdidos;
+    }
+
+    public void setPperdidos(int pperdidos) {
+        this.pperdidos = pperdidos;
+    }
+
+    public int getPtotales() {
+        return ptotales;
+    }
+
+    public void setPtotales(int ptotales) {
+        this.ptotales = ptotales;
+    }
+    
+    
+}
